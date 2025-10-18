c7ee2767d1fd9f5a3434461d7b11bf6d8ee7862f
jeta9939 <jeta9939@colorado.edu>
2025-10-17 23:28:50 -0600

    Add files via upload

---
+++ Taylor_MadLib.py
@@ -0,0 +1,13 @@
+#Howdy madlib enthusiasts! to play my game, run the code and fill in the blanks (press enter for next word), after filling in all your words, the Spooky story will be complete!
+
+hallowsEveAdjective = input("Adjective describing the night: ")
+spookieMovie = input("Name of a spooky movie: ")
+halloweenCostume = input("Jess' Halloween costume: ")
+favoriteCandy = input("Type of candy: ")
+onomatopoeia = input("Sound of the doorbell: ")
+trickOrTreater = input("Trick or Treater Halloween Costume: ")
+nounTreat = input("Random object: ")
+
+MadLib = f" Once upon a {hallowsEveAdjective} hallows eve, \n Jess was watching the classic spooky movie: {spookieMovie}. \n This year Jess decided to be a {halloweenCostume} for halloween. \n Unfortunately Jess was all out of Candy since she bought her favorite, {favoriteCandy}. \n All of a sudden, {onomatopoeia} went the doorbell, a trick or treater was at the door. \n Upon opening the door, a friendly {trickOrTreater} appeared. \n Jess gave them a {nounTreat}, since she was out of {favoriteCandy}."
+
+print (MadLib)
